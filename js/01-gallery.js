3a59ce5cc52bcc1c927c5bad3d2aa0715255539e
M-Tyshchenko <m.tish2406@gmail.com>
2023-06-11 22:30:02 +0300

    create gallery markup
    included basicLightbox js and css by cdn;
    create gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+//const gallaryListItemImage = document.querySelector('.gallery__image')
+const galleryItemMarkup = createGallaryItemsMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemMarkup);
+
+function createGallaryItemsMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                            />
+                    </a>
+                </li>`;
+    }).join('');
+}
+
+galleryList.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log(event.target.dataset.source);
+    
+});
+
+
