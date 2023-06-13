9f3c411b99f72a8887116122a255716ec98ab4dd
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-06-13 09:45:44 +0300

    Update 02-lightbox.js
    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,67 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
 
 const galleryList = document.querySelector('.gallery');
-
+const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
 
 function createGalleryItemsMarkup(galleryItems) {
+    
     return galleryItems.map(({preview, original, description}) => {
         return `<li class="gallery__item">
                     <a class="gallery__link" href="${original}">
-                        <img class="gallery__image" src="${preview}" alt="${description}" />
-                     </a>
+                        <img
+                            class="gallery__image"
+                            src="${preview}"                            
+                            alt="${description}"
+                            />
+                    </a>
                 </li>`;
     }).join('');
 }
 
-
-const galleryItemMarkup = createGalleryItemsMarkup(galleryItems);
-
 galleryList.insertAdjacentHTML('beforeend', galleryItemMarkup);
 
-galleryList.addEventListener('click', openModalHandler);
+galleryList.addEventListener('click', event => {
+   event.preventDefault();
+});
 
-function openModalHandler(event) {
-    event.preventDefault();
-
-    if (!event.target.classList.contains('gallery__image')) {
-        return;
-    }
-
-    const lightbox = new SimpleLightbox('.gallery a', { 
-        caption: true,
-    captionData: 'alt',
-    captionDelay: 250,
-    });
-    
- `<div>
-    <a href="${event.target.preview}"><img src="${event.target.original}" alt="${event.target.description}" title="${event.target.description}"/></a> 
-</div>`
-     
-  
-    window.addEventListener('keydown', closeModalHandler); 
-
-
-    function closeModalHandler(event) {
-        if (event.code !== 'Escape') {
-            return;
-        }
-        window.removeEventListener('keydown', closeModalHandler);
-    //     modalWindow.close();
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(galleryItems);
+ let lightbox = new SimpleLightbox('.gallery a', { 
+        captionsData: 'alt',
+        captionDelay: 250,
+     });
